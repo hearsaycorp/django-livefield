bdbc1231f3e5cc1dfe875a7e1d81645fb66f15e7
Akshay Shah <ashah@hearsaycorp.com>
2014-04-07 04:04:28 +0000

    Fix linting script
    Previously, this script would only fail if the last check failed.

--- lint.sh
+++ lint.sh
@@ -1,6 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 flake8 tests src setup.py
+if [ $? -ne 0 ]; then
+    echo "Flake8 linting failed."
+    exit 1
+fi
+
 pylint --rcfile=pylint.rc src/django_livefield
+if [ $? -ne 0 ]; then
+    echo "PyLing found errors in src/."
+    exit 1
+fi
+
 pylint --rcfile=pylint.rc tests
+if [ $? -ne 0 ]; then
+    echo "PyLint found errors in test/."
+    exit 1
+fi
+
 pylint --rcfile=pylint.rc setup.py
+if [ $? -ne 0 ]; then
+    echo "PyLint found errors in setup.py."
+    exit 1
+fi
