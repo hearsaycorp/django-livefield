14a768669b06709ffd38d42c2ffb97014793f7db
Dylan Verheul <dylan@dyve.net>
2014-07-07 16:53:33 +0200

    Introducing LiveGeoQuerySet

--- src/django_livefield/live_queryset.py
+++ src/django_livefield/live_queryset.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.db.models.query import GeoQuerySet
 from django.db import models
 
 
@@ -13,6 +14,32 @@ class LiveQuerySet(models.query.QuerySet):
     def hard_delete(self):
         # Default Django behavior.
         super(LiveQuerySet, self).delete()
+
+    def live(self):
+        return self.filter(live=True)
+
+    def non_dead(self):
+        return self.live()
+
+    def dead(self):
+        return self.filter(live__isnull=True)
+
+
+class LiveGeoQuerySet(GeoQuerySet):
+
+    def delete(self):
+        # Override Django's built-in default.
+        self.soft_delete()
+
+    def soft_delete(self):
+        self.update(live=False)
+
+    def undelete(self):
+        self.update(live=True)
+
+    def hard_delete(self):
+        # Default Django behavior.
+        super(LiveGeoQuerySet, self).delete()
 
     def live(self):
         return self.filter(live=True)
