f1696877d5ef0c6f9187afc364f5f983a75b6498
Adam DePue <adepue@hearsaycorp.com>
2015-12-03 19:10:10 +0000

    Handle removal of PassThroughManager

--- src/livefield/gis/managers.py
+++ src/livefield/gis/managers.py
@@ -1,15 +1,18 @@
-from model_utils.managers import PassThroughManager
+from django.db import models
 
 from .querysets import LiveGeoQuerySet
 
 
-class LiveGeoManager(PassThroughManager):
+LiveGeoManagerBase = models.Manager.from_queryset(LiveGeoQuerySet)
+
+
+class LiveGeoManager(LiveGeoManagerBase):
     # To also use this manager for FK lookups, see
     # https://docs.djangoproject.com/en/1.6/topics/db/managers/#manager-types.
 
-    def __init__(self, queryset_cls=LiveGeoQuerySet, include_soft_deleted=False, *args, **kwargs):
+    def __init__(self, include_soft_deleted=False, *args, **kwargs):
         self.include_soft_deleted = include_soft_deleted
-        super(LiveGeoManager, self).__init__(queryset_cls=queryset_cls, *args, **kwargs)
+        super(LiveGeoManager, self).__init__(*args, **kwargs)
 
     def get_queryset(self):
         qs = super(LiveGeoManager, self).get_queryset()
--- src/livefield/managers.py
+++ src/livefield/managers.py
@@ -1,15 +1,18 @@
-from model_utils.managers import PassThroughManager
+from django.db import models
 
 from .querysets import LiveQuerySet
 
 
-class LiveManager(PassThroughManager):
+LiveManagerBase = models.Manager.from_queryset(LiveQuerySet)
+
+
+class LiveManager(LiveManagerBase):
     # To also use this manager for FK lookups, see
     # https://docs.djangoproject.com/en/1.6/topics/db/managers/#manager-types.
 
-    def __init__(self, queryset_cls=LiveQuerySet, include_soft_deleted=False, *args, **kwargs):
+    def __init__(self, include_soft_deleted=False, *args, **kwargs):
         self.include_soft_deleted = include_soft_deleted
-        super(LiveManager, self).__init__(queryset_cls=queryset_cls, *args, **kwargs)
+        super(LiveManager, self).__init__(*args, **kwargs)
 
     def get_queryset(self):
         qs = super(LiveManager, self).get_queryset()
