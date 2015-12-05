465d12ce80d697ffd7e9ac446815565aa68bd710
Adam DePue <adepue@hearsaycorp.com>
2015-12-05 00:42:57 +0000

    Use correct Manager structure

--- src/livefield/gis/managers.py
+++ src/livefield/gis/managers.py
@@ -1,21 +1,20 @@
 from django.db import models
-
 from .querysets import LiveGeoQuerySet
 
 
-LiveGeoManagerBase = models.Manager.from_queryset(LiveGeoQuerySet)
-
-
-class LiveGeoManager(LiveGeoManagerBase):
+class LiveGeoManagerBase(models.Manager):
     # To also use this manager for FK lookups, see
     # https://docs.djangoproject.com/en/1.6/topics/db/managers/#manager-types.
 
     def __init__(self, include_soft_deleted=False, *args, **kwargs):
         self.include_soft_deleted = include_soft_deleted
-        super(LiveGeoManager, self).__init__(*args, **kwargs)  # pylint: disable=super-on-old-class
+        super(LiveGeoManagerBase, self).__init__(*args, **kwargs)  # pylint: disable=super-on-old-class
 
     def get_queryset(self):
-        qs = super(LiveGeoManager, self).get_queryset()  # pylint: disable=super-on-old-class
+        qs = super(LiveGeoManagerBase, self).get_queryset()  # pylint: disable=super-on-old-class
         if not self.include_soft_deleted:
             return qs.live()
         return qs
+
+
+LiveGeoManager = LiveGeoManagerBase.from_queryset(LiveGeoQuerySet)
